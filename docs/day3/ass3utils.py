a43b65a0af1bf4f4b46741441d0b9c0a9422490a
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2019-05-22 11:47:16 +0300

    Reduced size of dataset and default no iters
    Unsupervised training is taking too long!

--- docs/day3/ass3utils.py
+++ docs/day3/ass3utils.py
@@ -4,7 +4,7 @@ from nltk.tag import HiddenMarkovModelTrainer
 from nltk.probability import LidstoneProbDist
 
 
-def train_unsupervised(labeled_sents, unlabeled_sents, max_iterations=5):
+def train_unsupervised(labeled_sents, unlabeled_sents, max_iterations=3):
     symbols = unique_list(word for sent in labeled_sents for word, tag in sent)
     # Extend symbols with those in the unlabelled set
     symbols = unique_list(symbols + unique_list(word for sent in unlabeled_sents for word in sent))
@@ -18,7 +18,9 @@ def train_unsupervised(labeled_sents, unlabeled_sents, max_iterations=5):
     unlabeled_sents = [
         [(word, None) for word in sent] for sent in unlabeled_sents
     ]
-    print("Unsupervised training ({} sentences)".format(len(unlabeled_sents)))
+    print("Unsupervised training ({} sentences) for up to {} iterations".format(
+        len(unlabeled_sents)), max_iterations
+    )
     hmm = trainer.train_unsupervised(
         unlabeled_sents, model=hmm, max_iterations=max_iterations, verbose=True
     )
